d3cef5b71beb1cb15d9f2ebbae7323e08f8b609b
Bryan <bryanlmei@laposte.net>
2019-10-11 15:29:49 +0200

    security finish

--- Classy/demo-sécurité/1injection.php
+++ Classy/demo-sécurité/1injection.php
@@ -21,6 +21,10 @@ EOT;
 echo $html;
 
 $test = '"><script>console.log("coucou 2")</script><span class="';
-$html2 = '<a href="' . $test . '">texte</a>';
+$html2 = '<a href="' . htmlspecialchars($test) . '">texte</a>';
 
 echo $html2;
+
+$recipient = "pbreteche@dawan.fr\n\rbcc:autremail@dawan.fr";
+
+mail($recipient, 'sujet', 'message');
